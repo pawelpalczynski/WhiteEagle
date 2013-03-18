d3eb31a4e5c32c3aefacc9080fd5af9bc03b13d6
Paweł Palczyński <p.j.palczynski@gmail.com>
2013-03-18 02:11:49 +0000

    Initial commit

---
+++ src/com/pauson/whiteeagle/WhiteEagle.java
@@ -0,0 +1,34 @@
+package com.pauson.whiteeagle;
+
+import com.badlogic.gdx.ApplicationListener;
+
+public class WhiteEagle implements ApplicationListener {
+	
+	
+	@Override
+	public void create() {		
+		
+	}
+
+	@Override
+	public void dispose() {
+		
+	}
+
+	@Override
+	public void render() {		
+		
+	}
+
+	@Override
+	public void resize(int width, int height) {
+	}
+
+	@Override
+	public void pause() {
+	}
+
+	@Override
+	public void resume() {
+	}
+}
